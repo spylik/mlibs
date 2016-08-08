1b53f1beb5781816bb0bbc4d29ecd7d10142cf3b
Oleksii D. Semilietov <spylik@gmail.com>
2016-08-08 11:52:13 +0300

    add tutils

---
+++ src/tutils.erl
@@ -0,0 +1,25 @@
+-module(tutils).
+
+-compile(export_all).
+
+% recieve loop
+recieve_loop(Acc) ->
+    recieve_loop(Acc, 15).
+
+recieve_loop(Acc, Timeout) ->
+    receive   
+        {got, Data} -> recieve_loop([Data|Acc],Timeout)
+        after Timeout -> Acc
+    end.
+
+% waiting loop
+wait_msg_loop(SendToPid) ->
+    receive
+        stop -> true;
+        Msg ->
+            SendToPid ! {got, Msg},
+            wait_msg_loop(SendToPid)
+    end.
+
+disable_output() ->
+    error_logger:tty(false).
