a9f90b77fef482c66f07e12783a992fd4ac98c84
Oleksii D. Semilietov <spylik@gmail.com>
2016-08-14 08:07:16 +0300

    tutils:spawn_wait_loop/1

--- src/tutils.erl
+++ src/tutils.erl
@@ -3,7 +3,6 @@
 -compile(export_all).
 
 % recieve loop
-
 recieve_loop() -> recieve_loop([], 15). 
 
 recieve_loop(Acc) ->
@@ -14,6 +13,8 @@ recieve_loop(Acc, Timeout) ->
         {got, Data} -> recieve_loop([Data|Acc],Timeout)
         after Timeout -> Acc
     end.
+
+spawn_wait_loop(SendToPid) -> spawn_link(?MODULE, wait_msg_loop, [SendToPid]).
 
 % waiting loop
 wait_msg_loop(SendToPid) ->
