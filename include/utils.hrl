b90664b7f3bf07a3df05971e25f74d3fb866b96a
Oleksii D. Semilietov <spylik@gmail.com>
2016-10-22 14:09:12 +0300

    wait_for with output

--- include/utils.hrl
+++ include/utils.hrl
@@ -24,6 +24,9 @@
     ).
 
 % info macros (will be available both in development and release mode)
+-define(info(Msg),
+        error_logger:info_msg("(~p)~p: ~p", [?LINE,?MODULE,Msg])
+    ).
 -define(info(Msg,Arg),
         error_logger:info_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg))
     ).
