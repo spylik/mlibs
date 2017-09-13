71cf29c4258a6097d0cce23253e6811eb2509de3
Oleksii D. Semilietov <spylik@gmail.com>
2017-09-13 18:27:02 +0300

    draft for newver

--- bin/newver.sh
+++ bin/newver.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# sh-driven cross-vcs hook for automatically update version for Erlang and Elixir projects.
+#
+# It uses the following strategy:
+# - patch part of version updates during each commit.
+# - minor part of version updates during each merge.
+# - major part updates manually only. for update run path_to_newver/newver.sh -M
+#
+# Currently support git and mercurial as source version control system
+
 while getopts ":Mmp" Option
 do
   case $Option in
@@ -9,16 +18,44 @@ do
   esac
 done
 
-for file in $(find src -type f -name \*.app.src); do
-	old="$(cat $file | grep -o '{vsn\s*,\s*"\d*.\d*.\d*"}' | grep -o '"\d*.\d*.\d*"' | sed -e 's/^"//' -e 's/"$//')"
+nv() {
 	ar=( ${old//./ } )
 	if [ ! -z $major ]; then ((ar[0]++));ar[1]=0;ar[2]=0;fi
 	if [ ! -z $minor ]; then ((ar[1]++));ar[2]=0;fi
 	if [ ! -z $patch ]; then ((ar[2]++));fi
-	new="${ar[0]}.${ar[1]}.${ar[2]}"
-	echo "Patch file $file. Old vsn: $old, new vsn: $new"
+	echo "${ar[0]}.${ar[1]}.${ar[2]}"
+}
+
+# Determine which version control system we using for this project
+add_to_repo(){
+	if [ -d ".git" ]; then
+		git add $file
+	fi
+	if [ -d ".hg" ]; then
+		hg commit --amend $file
+	fi
+}
+
+if [ -e "mix.exs" ]; then
+	file="mix.exs"
+	echo "Found mix.exs (Elixir project)"
+	old=$(cat $file | awk '/version:/{print $2;}' | tr -d '",')
+	new=$(nv $old)
+	echo "Patching file $file. Old vsn: $old, new vsn: $new"
 	sed -i.bak s/$old/$new/g $file
 	rm $file.bak
-	git add $file
-	if [ -e "relx.config" ]; then echo "Patch file relx.config. New vsn: $new"; sed -i.bak s/$old/$new/g relx.config;rm relx.config.bak;git add relx.config;fi
-done;
+	add_to_repo $file
+else
+	if [ -d "src" ]; then
+		echo "Found src folder.. (probably Erlang project)"
+		for file in $(find src -type f -name \*.app.src); do
+			old="$(cat $file | grep -o '{vsn\s*,\s*"\d*.\d*.\d*"}' | grep -o '"\d*.\d*.\d*"' | sed -e 's/^"//' -e 's/"$//')"
+			new=$(nv $old)
+			echo "Patching file $file. Old vsn: $old, new vsn: $new"
+			sed -i.bak s/$old/$new/g $file
+			rm $file.bak
+			add_to_repo $file
+			if [ -e "relx.config" ]; then echo "Patch file relx.config. New vsn: $new"; sed -i.bak s/$old/$new/g relx.config;rm relx.config.bak;add_to_repo relx.config;fi
+		done;
+	fi
+fi
