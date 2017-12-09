d9b33f010ac9bcb45a70f98a931ccd52c9631423
Oleksii D. Semilietov <spylik@gmail.com>
2017-12-09 11:12:45 +0200

    fix dialyzer issue when we use macros and passing predictable type

--- include/utils.hrl
+++ include/utils.hrl
@@ -3,15 +3,11 @@
 -ifndef(release).
     -define(here, error_logger:info_msg("(~p)~p: we are here", [?LINE,?MODULE]), true).
     -define(dump_to_file(Data, Filename),
-        file:write_file(Filename, io_lib:fwrite("~s~n",[Data]))
+        ok = file:write_file(Filename, io_lib:fwrite("~s~n",[Data]))
     ).
     -define(debug(Msg),
-        case is_binary(Msg) of
-            false ->
-                error_logger:info_msg("(~p)~p: ~p is ~p", [?LINE,?MODULE,??Msg,Msg]);
-            true ->
-                error_logger:info_msg("(~p)~p: ~p is ~s", [?LINE,?MODULE,??Msg,Msg])
-        end, true).
+        error_logger:info_msg("(~p)~p: ~p is ~p", [?LINE,?MODULE,??Msg,Msg]), true
+    ).
     -define(debug(Msg,Arg),
         error_logger:info_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg)), true
     ).
@@ -24,37 +20,27 @@
 
 % warning macros (will be available both in development and release mode)
 -define(warning(Msg),
-        error_logger:warning_msg("(~p)~p: ~p", [?LINE,?MODULE,Msg])
+        error_logger:warning_msg("(~p)~p: ~p", [?LINE,?MODULE,Msg]), true
     ).
 -define(warning(Msg,Arg),
-        error_logger:warning_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg))
+        error_logger:warning_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg)), true
     ).
 
 % info macros (will be available both in development and release mode)
 -define(info(Msg),
-        case is_list(Msg) of
-            false ->
-                error_logger:info_msg("(~p)~p: ~p", [?LINE,?MODULE,Msg]);
-            true ->
-                error_logger:info_msg("(~p)~p: ~s", [?LINE,?MODULE,Msg])
-        end
+        error_logger:info_msg("(~p)~p: ~s", [?LINE,?MODULE,Msg]), true
     ).
 -define(info(Msg,Arg),
-        error_logger:info_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg))
+        error_logger:info_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg)), true
     ).
 
 % error macros (will be available both in development and release mode)
 -define(error(Msg),
-        case is_list(Msg) of
-            false ->
-                error_logger:error_msg("(~p)~p: ~p", [?LINE,?MODULE,Msg]);
-            true ->
-                error_logger:error_msg("(~p)~p: ~s", [?LINE,?MODULE,Msg])
-        end
+        error_logger:error_msg("(~p)~p: ~s", [?LINE,?MODULE,Msg]), true
     ).
 
 -define(error(Msg,Arg),
-        error_logger:error_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg))
+        error_logger:error_msg(lists:concat(["(~p)~p: ", Msg]), lists:append([?LINE,?MODULE], Arg)), true
     ).
 
 % got undefined message with standart format
@@ -64,7 +50,8 @@
             [] -> unregistered;
             {registered_name, Name} -> Name
         end,
-        error_logger:warning_msg("(~p)~p:~p/~p (~p, ~p) something undefined:~n~p", [?LINE,M,F,A,self(),RegisteredName,Arg])
+        error_logger:warning_msg("(~p)~p:~p/~p (~p, ~p) something undefined:~n~p", [?LINE,M,F,A,self(),RegisteredName,Arg]),
+        true
     ).
 % got undefined message with custom format
 -define(undefined(Msg,Arg),
@@ -73,5 +60,6 @@
             [] -> unregistered;
             {registered_name, Name} -> Name
         end,
-        error_logger:warning_msg(lists:concat(["(~p)~p:~p/~p (~p, ~p)", Msg]), [?LINE,M,F,A,self(),RegisteredName,Arg])
+        error_logger:warning_msg(lists:concat(["(~p)~p:~p/~p (~p, ~p)", Msg]), [?LINE,M,F,A,self(),RegisteredName,Arg]),
+        true
     ).
