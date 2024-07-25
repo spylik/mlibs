cc3ce975f380c3fbd96e0743a935218e692a3a5e
Oleksii Semilietov <spylik@gmail.com>
2024-07-25 10:46:58 +0700

    fix

--- bin/ssh_wrapper.sh
+++ bin/ssh_wrapper.sh
@@ -1,6 +1,7 @@
 if [ ! -z $SSH_CONFIG_FILE ] ; then
     echo "$SSH_CONFIG_FILE in use"
+    exec ssh -F "$SSH_CONFIG_FILE" "$@"
     exec ssh -F $SSH_CONFIG_FILE $argv
 else
-    exec ssh $argv
+    exec ssh "$@"
 fi
